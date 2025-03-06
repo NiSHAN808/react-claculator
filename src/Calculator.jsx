299443570223fb7dd7b739dd8e55098f80bb6c8a
NiSHAN808 <nishan4b@gmail.com>
2025-03-06 13:29:14 +0545

    one line 24 olny add is added other operation is remaning

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,20 +1,49 @@
 import './CalculatorStyle.css'
+import { useState,useRef } from 'react';
 
 
 
+function Calculator(){
+var first_number = useRef(undefined);
+let sign_positiin=useRef(0);
+ let [input,setInput]=useState("");
 
-function Calculator(e){
    function handleBtnClick(index){
-    console.log(buttons[index])
-  
+    
+    if(typeof(buttons[index])==="string"){
+      
+
+        if(first_number.current === undefined){
+          first_number.current=Number(input);
+          
+          sign_positiin.current=input.length+1;
+          setInput( input + buttons[index]);
+        
+        }else{
+         
+           let x= first_number.current + Number(input.slice(sign_positiin.current,input.length));
+           first_number.current=x;
+          // console.log("x=",x,"    y=  ",y, "     i=", sign_positiin.current);
+           setInput(x);
+        }
+
+
+
+    }else{
+      
+    setInput( input + buttons[index]);
+    }
    }
- let buttons=["C","()","%","/","7","8","9","X","4","5","6","-","1","2","3","+","+/-","0",".","="];
+
+
+
+ let buttons=["C","()","%","/",7,8,9,"X",4,5,6,"-",1,2,3,"+","+/-",0,".","="];
 
 
 
 return(
     <>
-    <div className="outBox"></div>
+    <div  className="outBox">{input}</div>
     <div className="buttonsOutBox">
          {buttons.map((button,index)=>
              <div className="btn" key={index} 
